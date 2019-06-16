0dbd17ed77a52236e83a004205012a71d52b6d5c
A Reynolds <afr5057@gmail.com>
2019-06-15 21:02:07 -0500

    add public api

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// API key
-const API_KEY = "enter your api key here";
+// API key below is a public generic token. You can replace with your own.
+const API_KEY = "pk.eyJ1IjoiYWZyNTA1NyIsImEiOiJjandneHZvdzIyN3A3NDFxbmR0M2M4cXBrIn0.fzN0hwtoO4Dyxe7i4LRrHw";
